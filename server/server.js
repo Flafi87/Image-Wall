2f33ca2ac30f2da05ed14a679a883f06f2985b16
unknown <gflafi@gmail.com>
2019-04-23 22:52:38 +0200

    Clean up ignored files

--- server/server.js
+++ server/server.js
@@ -27,16 +27,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 
 app.use("/uploads", express.static("uploads"));
 
-// Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
